3b4b4cb0da8a31b8da495c13eb5d32601958d566
Masterflopes <up201605337@fe.up.pt>
2018-04-27 17:28:46 +0100

    first files for the final project

---
+++ Trabalho Prático 2/client.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+  if (argc < 2) {
+    printf("Usage: %s <time_out> <num_wanted_seats> <pref_seat_list>\n", argv[0]);
+    return 1;
+  }
+  if (argc < 4){
+    printf("Missing arguments!\n");
+    return 1;
+  }
+
+  int time_out = atoi(argv[1]);
+  int num_wanted_seats = atoi(argv[2]);
+
+//////////////////////////////////////////////////////////
+// Code to get pref_seat_list
+  char newString[sizeof(argv[3])][sizeof(argv[3])];
+  int i, j=0, ctr=0;
+  for(i=0; i <= (strlen(argv[3])); i++){
+    if(argv[3][i] == ' ' || argv[3][i]=='\0'){
+      newString[ctr][j]='\0';
+      ctr++;
+      j=0;
+    }
+    else{
+      newString[ctr][j]= argv[3][i];
+      j++;
+    }
+  }
+  int pref_seat_list[ctr];
+  for(i=0;i < ctr;i++){
+    pref_seat_list[i] = atoi(newString[i]);
+  }
+//////////////////////////////////////////////////////////
+
+
+
+
+
+
+  return 0;
+}
---
+++ Trabalho Prático 2/server.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char* argv[]){
+  if (argc < 2) {
+    printf("Usage: %s <num_room_seats> <num_ticket_offcies> <open_time>\n", argv[0]);
+    return 1;
+  }
+  if (argc < 4){
+    printf("Missing arguments!\n");
+    return 1;
+  }
+
+  int num_room_seats = atoi(argv[1]);
+  int num_ticket_offcies = atoi(argv[2]);
+  int open_time = atoi(argv[3]);
+
+
+
+
+
+
+
+  return 0;
+}
